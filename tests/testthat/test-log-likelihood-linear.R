e963e58951cc8ae2db03528df9d427bc5270e764
wli51 <wli51@jh.edu>
2023-02-22 15:52:40 -0500

    modify call to approx_grad function in test, now using anonymous functions for better clarity

--- tests/testthat/test-log-likelihood-linear.R
+++ tests/testthat/test-log-likelihood-linear.R
@@ -7,7 +7,10 @@ test_that("log_likelihood_linear_gradient against numerical gradient", {
   random_c <- rnorm(n_pred)
 
   analytical_gradient <- log_likelihood_gradient_linear(random_c, design, outcome, noise_var=1)
-  numerical_gradient <- approx_grad(log_likelihood_linear, random_c, design, outcome, noise_var=1)
+  # numerical_gradient <- approx_grad(log_likelihood_linear, random_c, design, outcome, noise_var=1)
+  numerical_gradient <-
+    approx_grad(func = function(x) log_likelihood_linear(coef=x, x=design, y=outcome, noise_var=1),
+                x=random_c)
 
   expect_true(are_all_close(
     analytical_gradient, numerical_gradient, abs_tol = 1e-2, rel_tol = 1e-2
