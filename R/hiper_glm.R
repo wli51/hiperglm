f7c0bf963f2abc1ba3d3473eb04ab7bc92ac8c5b
wli51 <wli51@jh.edu>
2023-02-21 22:30:52 -0500

    updated hiper_glm function so it accepts option argument as a list

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,5 +1,5 @@
 #' @export
-hiper_glm <- function(design, outcome, model="linear") {
+hiper_glm <- function(design, outcome, model="linear", option = list()) {
 
   supported_model <- c("linear", "logit")
 
@@ -7,7 +7,23 @@ hiper_glm <- function(design, outcome, model="linear") {
     stop(sprintf("The model %s is not supported.", model))
   }
 
-  warning("`hiper_glm` is yet to be implemented.")
+  if (!is.null(option[["mle_solver"]])) {
+
+    if (option[["mle_solver"]] == "BFGS") {
+      message("Using BFGS\n")
+
+    } else {
+      warning("No current plans for adding solver options other than pseudo-inverse and BFGS.")
+      stop()
+    }
+
+
+  } else {
+    message("No mle_solver option specified, using pseudo inverse by default\n")
+
+  }
+
+  # warning("`hiper_glm` is yet to be implemented.")
 
   # TODO: implement find MLE.
   hglm_out <- list()
