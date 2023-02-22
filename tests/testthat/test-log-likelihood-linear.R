9100f5f8dd4b431a69a1e104a2504b7d5548c63e
wli51 <wli51@jh.edu>
2023-02-21 23:04:34 -0500

    add test file to test whether log_likelihood_linear_gradient function's output coincides with numerical gradient

---
+++ tests/testthat/test-log-likelihood-linear.R
@@ -0,0 +1,15 @@
+test_that("log_likelihood_linear_gradient against numerical gradient", {
+
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 2883)
+  design <- data$design; outcome <- data$outcome
+
+  random_c <- rnorm(n_pred)
+
+  analytical_gradient <- log_likelihood_gradient_linear(random_c, design, outcome, noise_var=1)
+  numerical_gradient <- approx_grad(log_likelihood_linear, random_c, design, outcome, noise_var=1)
+
+  expect_true(are_all_close(
+    analytical_gradient, numerical_gradient, abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
