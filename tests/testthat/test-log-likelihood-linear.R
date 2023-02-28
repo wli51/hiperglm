19ee7b508e7f79b37d67224ef542c84103bd2529
wli51 <wli51@jh.edu>
2023-02-28 00:03:03 -0500

    replace random initialization of coefficients with fixed, also make tolerances smaller in the test case

--- tests/testthat/test-log-likelihood-linear.R
+++ tests/testthat/test-log-likelihood-linear.R
@@ -4,15 +4,14 @@ test_that("log_likelihood_linear_gradient against numerical gradient", {
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 2883)
   design <- data$design; outcome <- data$outcome
 
-  random_c <- rnorm(n_pred)
+  start_c <- rep(1, n_pred)
 
-  analytical_gradient <- log_likelihood_gradient_linear(random_c, design, outcome, noise_var=1)
-  # numerical_gradient <- approx_grad(log_likelihood_linear, random_c, design, outcome, noise_var=1)
+  analytical_gradient <- log_likelihood_gradient_linear(start_c, design, outcome, noise_var=1)
   numerical_gradient <-
     approx_grad(func = function(x) log_likelihood_linear(coef=x, x=design, y=outcome, noise_var=1),
-                x=random_c)
+                x=start_c)
 
   expect_true(are_all_close(
-    analytical_gradient, numerical_gradient, abs_tol = 1e-2, rel_tol = 1e-2
+    analytical_gradient, numerical_gradient, abs_tol = 1e-3, rel_tol = 1e-3
   ))
 })
