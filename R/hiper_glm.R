cafbd4b28d38e4217ae08a14baed92eed316a8e9
wli51 <wli51@jh.edu>
2023-02-28 13:51:14 -0500

    bring back the if else statement because swtich is kind of complicated and restrictive here

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -14,12 +14,15 @@ hiper_glm <-
     option$mle_solver <-
       match.arg(option$mle_solver, c("pseudo inverse", "BFGS"))
 
-    coef_estimate <- switch (
-      option$mle_solver,
-      "pseudo inverse" = linear.mle.pseudo_inverse(design, outcome),
-      "BFGS" = coef_estimate <- linear.mle.BFGS(design, outcome),
-      NULL
-    )
+    coef_estimate <- NULL
+
+    if (option$mle_solver == "pseudo inverse") {
+      coef_estimate <- linear.mle.pseudo_inverse(design, outcome)
+    } else if (option$mle_solver == "BFGS") {
+      coef_estimate <- linear.mle.BFGS(design, outcome)
+    } else {
+      stop("No current plans for adding solver options other than pseudo inverse and BFGS.")
+    }
 
     hglm_out <- list()
     class(hglm_out) <- "hglm"
