af5c94d17e76782328d66c12fad91a0743366249
wli51 <wli51@jh.edu>
2023-02-27 23:28:10 -0500

    separate out mle solvers for linear system to a separate file

---
+++ R/mle_solvers.R
@@ -0,0 +1,15 @@
+linear.mle.pseudo_inverse <- function(design, outcome) {
+
+  chol_solve_linear_system(A = t(design) %*% design, b=t(design) %*% outcome)
+
+}
+
+linear.mle.BFGS <- function(design, outcome) {
+
+  op <- stats::optim(par = rnorm(dim(design)[2]),
+                     fn = function(par) log_likelihood_linear(par, design, outcome),
+                     gr = function(par) log_likelihood_gradient_linear(par, design, outcome),
+                     control = list(fnscale=-1))
+
+  matrix(op$par, ncol=1)
+}
