1e7625bd95b2ca11cca444105558a5a29e56be55
wli51 <wli51@jh.edu>
2023-03-06 00:18:05 -0500

    setup new test for logit model

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -22,3 +22,16 @@ test_that("linalg and optim least-sq coincide", {
     rel_tol = 1e-2
   ))
 })
+
+test_that("newton and bfgs outputs coincide on logit model", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_bfgs_out <- hiper_glm(
+    design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
+  )
+  expect_true(are_all_close(
+    coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
