61e251c73d37bb60f4c483bc24de56df36051357
wli51 <wli51@jh.edu>
2023-03-06 18:24:43 -0500

    adding convergence criteria to newton method

--- R/logistic_mle.R
+++ R/logistic_mle.R
@@ -14,7 +14,7 @@ logistic.log_likelihood.gradient <- function(coef, x, y) {
 
 #'
 logistic.fun <- function(coef, x) {
-  1/(1+exp(-1 * x %*% coef))
+  1 / (1 + exp(-1 * x %*% coef))
 }
 
 #'
@@ -33,35 +33,53 @@ logistic.mle.BFGS <- function(design, outcome) {
 
 #'
 logistic.log_likelihood.hessian <- function(coef, x, y) {
-  -1*t(x) %*% weight_matrix(coef, x) %*% x
+  -1 * t(x) %*% weight_matrix(coef, x) %*% x
 }
 
 #'
 weight_matrix <- function(coef, x) {
   n <- dim(x)[1]
-  weight <- matrix(0, nrow=n, ncol=n)
+  weight <- matrix(0, nrow = n, ncol = n)
 
   for (i in 1:n) {
-    p = logistic.fun(coef, x[i,])
-    weight[i,i] = p * (1-p)
+    p = logistic.fun(coef, x[i, ])
+    weight[i, i] = p * (1 - p)
   }
   weight
 }
 
-logistic.mle.newton <- function(design, outcome, it=1000, start_coef = NULL, qr_tol = 1e-5) {
+logistic.mle.newton <-
+  function(design,
+           outcome,
+           max_it = 1000,
+           start_coef = NULL) {
+    if (is.null(start_coef)) {
+      coef <- rep(0, dim(design)[2])
+    } else {
+      coef <- start_coef
+    }
+    # 2 times difference in log likelihood under null model follows chi-sq
+    # distribution, using mean of chi-sq with df1 here as proxy for negligible
+    # increase in log likelihood
+    negligible_log_lik_increment <- 1
+    conservative_multiplier <- 0.05
+    convergence_log_lik_tolerance <- negligible_log_lik_increment *
+      conservative_multiplier
 
-  if (is.null(start_coef)) {
-    coef <- rep(0, dim(design)[2])
-  } else {
-    coef <- start_coef
+    for (i in 1:max_it) {
+      log_lik_prev <- logistic.log_likelihood(coef, design, outcome)
+      hessian <-
+        logistic.log_likelihood.hessian(coef, design, outcome)
+      gradient <-
+        logistic.log_likelihood.gradient(coef, design, outcome)
+      coef <- coef - matlib::inv(hessian) %*% gradient
+      log_lik_curr <- logistic.log_likelihood(coef, design, outcome)
+      if (log_lik_curr - log_lik_prev < convergence_log_lik_tolerance) {
+        break
+      }
+      log_lik_prev <- log_lik_curr
+    }
+
+    matrix(coef, ncol = 1)
   }
 
-  for (i in 1:it) {
-    hessian <- logistic.log_likelihood.hessian(coef, design, outcome)
-    gradient <- logistic.log_likelihood.gradient(coef, design, outcome)
-
-    coef <- coef - matlib::inv(hessian) %*% gradient
-  }
-
-  matrix(coef, ncol=1)
-}
