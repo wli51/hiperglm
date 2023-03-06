4da84eb5c85ecadef2de70f67d49007949e7d07a
wli51 <wli51@jh.edu>
2023-03-06 00:37:29 -0500

    add test for correctness of analytical gradient for logistic likelihood

---
+++ tests/testthat/test-log_likelihood_logistic_gradient.R
@@ -0,0 +1,30 @@
+test_that("log_likelihood_logistic_gradient against numerical gradient", {
+  n_obs <- 32
+  n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design
+  outcome <- data$outcome
+
+  start_c <- rep(1, n_pred)
+
+  analytical_gradient <-
+    logistic.log_likelihood.gradient(start_c, design, outcome)
+  numerical_gradient <-
+    approx_grad(
+      func = function(x)
+        logistic.log_likelihood(
+          coef = x,
+          x = design,
+          y = outcome),
+      x = start_c
+    )
+
+  expect_true(
+    are_all_close(
+      analytical_gradient,
+      numerical_gradient,
+      abs_tol = 1e-3,
+      rel_tol = 1e-3
+    )
+  )
+})
